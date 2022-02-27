32ec5ce7911d8ca51274e30c706c832d43da3e07
Salim Mvoungui <salim.mvoungui@gmail.com>
2022-02-27 01:35:41 +0100

    showpassword

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useState } from "react";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { AiFillEye } from "react-icons/ai";
 
 const LoginInput = () => {
+  const [visible, setVisibility] = useState(false);
+  const icon = <AiFillEye />;
+  const inputType = visible ? "text" : "password";
   return (
     <div className="register-form">
       <h3>Login</h3>
@@ -9,7 +15,13 @@ const LoginInput = () => {
         <input type="text" name="username" placeholder="Username" />
         <br />
 
-        <input type="password" name="password" placeholder="Password" />
+        <input type={inputType} name="password" placeholder="Password" />
+        <span
+          className="password-icon"
+          onClick={() => setVisibility((visibility) => !visibility)}
+        >
+          {icon}
+        </span>
         <br />
 
         <br />
